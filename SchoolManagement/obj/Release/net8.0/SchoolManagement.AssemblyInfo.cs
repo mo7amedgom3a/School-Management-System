ed8055578700b6bef78ca00e2b7f336f2aafd959
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-07-14 05:56:39 +0300

    update ip address

--- SchoolManagement/obj/Release/net8.0/SchoolManagement.AssemblyInfo.cs
+++ SchoolManagement/obj/Release/net8.0/SchoolManagement.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11c76514b829d26d8b360aa1c2d1c3eeeb965bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement")]
